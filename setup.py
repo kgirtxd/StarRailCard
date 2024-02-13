7a57820bff57e4e5975fb97f7ca56ca0b280a8c5
Konard <retributionx99@gmail.com>
2024-02-12 20:39:12 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='starrailcard',
-    version='1.2.6',
+    version='1.3.0k',
     description='Module for generating Honkai Star Rail character cards',
     author='None',
     packages=find_packages(),
